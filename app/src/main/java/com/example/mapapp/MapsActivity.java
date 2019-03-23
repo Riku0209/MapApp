c4d22b1677cd5e5271c59387398e3df2bbae710c
Riku0209 <rikugoto0209@icloud.com>
2019-03-23 17:32:52 +0900

    "を押したね”を追加

--- app/src/main/java/com/example/mapapp/MapsActivity.java
+++ app/src/main/java/com/example/mapapp/MapsActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -52,13 +53,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         options.position(aomori);
         options.title("どこかです");
         options.snippet("ここではないどこか");
-        Marker marker = mMap.addMarker(options);
+        final Marker marker = mMap.addMarker(options);
         marker.showInfoWindow();
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,new  String[]{
+            ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION
-            },1);
+            }, 1);
             return;
         }
         mMap.setMyLocationEnabled(true);
@@ -66,9 +67,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onMapLongClick(LatLng latLng) {
                 mMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title("New Marker")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                        .position(latLng)
+                        .title("New Marker")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+            }
+        });
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Toast.makeText(MapsActivity.this, marker.getTitle() + "を押したね", Toast.LENGTH_SHORT).show();
             }
         });
     }
